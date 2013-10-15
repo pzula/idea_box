5b6d526603e8b1edd41a5be3b67d510e9f2c9166
Persa Zula <persa@persazula.com>
2013-10-15 09:53:20 -0600

    First iteration with ideas saving to YAML database

---
+++ app.rb
@@ -0,0 +1,26 @@
+require_relative 'idea'
+
+class IdeaBoxApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    # 1. Create an idea based on the form parameters
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+
+    # 2. Store it
+    idea.save
+
+    # 3. Send us back to the index page to see all ideas
+    redirect '/'
+  end
+
+  not_found do
+    erb :error
+  end
+end
