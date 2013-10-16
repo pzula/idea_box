78e2af96851146234c5466a79286ede40f1d7dcd
Persa Zula <persa@persazula.com>
2013-10-15 21:14:53 -0600

    Setting the ID within the Idea objects. Also allows for sorting based on rank.

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -1,11 +1,12 @@
 class Idea
 
-  attr_reader :title, :description, :rank
+  attr_reader :title, :description, :rank, :id
 
   def initialize(attributes = {})
     @title = attributes["title"]
     @description = attributes["description"]
     @rank = attributes["rank"] || 0
+    @id = attributes["id"]
   end
 
   def save
@@ -20,9 +21,12 @@ class Idea
     }
   end
 
-
   def like!
     @rank += 1
+  end
+
+  def <=>(other)
+    other.rank <=> rank
   end
 
 end
