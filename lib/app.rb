7180c2a0a7473be0591d2dd4b5b9efbef48a16f3
Persa Zula <persa@persazula.com>
2013-10-24 14:05:41 -0600

    Constrains the ideas routes

--- lib/app.rb
+++ lib/app.rb
@@ -6,6 +6,16 @@ class IdeaBoxApp < Sinatra::Base
   set :method_override, true
   set :root, 'lib/app'
 
+  helpers do
+    def idea_path(idea=nil)
+      if idea
+        "ideas/#{idea.id}"
+      else
+        "ideas"
+      end
+    end
+  end
+
   not_found do
     erb :error
   end
@@ -18,21 +28,37 @@ class IdeaBoxApp < Sinatra::Base
     erb :index, locals: {ideas: IdeaStore.all.sort, idea: Idea.new, tags: IdeaStore.all_tags}
   end
 
-  post '/' do
+  post '/ideas' do
     IdeaStore.create(params[:idea])
     redirect '/'
   end
 
-  delete '/:id' do |id|
+  delete '/ideas/:id' do |id|
     IdeaStore.delete(id.to_i)
     redirect '/'
   end
 
-  get '/:id/edit' do |id|
+  get '/ideas/:id/edit' do |id|
     idea = IdeaStore.find(id.to_i)
     erb :edit, locals: {idea: idea}
   end
 
+  put '/ideas/:id' do |id|
+    IdeaStore.update(id.to_i, params[:idea])
+    redirect '/'
+  end
+
+  post '/ideas/:id/like' do |id|
+    idea = IdeaStore.find(id.to_i)
+    idea.like!
+    IdeaStore.update(id.to_i, idea.to_h)
+    redirect '/'
+  end
+
+  get '/ideas/:id' do |id|
+    idea = IdeaStore.find(id.to_i)
+    erb :idea, locals: {idea: idea}
+  end
 
   get '/tag/:tag' do |tag|
     ideas = IdeaStore.find_all_by_tag(tag)
@@ -44,18 +70,6 @@ class IdeaBoxApp < Sinatra::Base
     erb :search, locals: {ideas: ideas, phrase: params[:phrase]}
   end
 
-  put '/:id' do |id|
-    IdeaStore.update(id.to_i, params[:idea])
-    redirect '/'
-  end
-
-  post '/:id/like' do |id|
-    idea = IdeaStore.find(id.to_i)
-    idea.like!
-    IdeaStore.update(id.to_i, idea.to_h)
-    redirect '/'
-  end
-
   get '/sms' do
     title, description_tags = params[:Body].split(" :: ")
     description, tags = description_tags.split(" # ")
@@ -65,12 +79,4 @@ class IdeaBoxApp < Sinatra::Base
     redirect '/'
     # send a text to the number using title :: description # tag, tag, tag
   end
-
-  get '/:id' do |id|
-    idea = IdeaStore.find(id.to_i)
-    erb :idea, locals: {idea: idea}
-    # put a restriction on id --- a greedy route
-  end
-
-
 end
